f8bd2b61b1a2d9d301bcb2b83686c4ec3cc9bfae
Jamnic98 <jamie@qudo.ai>
2024-12-05 14:20:21 +0000

    refactor

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,32 @@ import SupportWorkerCard from './components/SupportWorkerCard'
 const baseUrl = 'http://localhost:8080'
 const url = baseUrl + '/getSupportWorkerVisits'
 
+const transformData = (data) => {
+  // transform the data by collating worker's visits by supportWorkerId
+  const transformedData = data.reduce((result, visit) => {
+    const { supportWorkerId, name, contractedHours, avatar, ...visitData } =
+      visit
+
+    // create new worker obj if none exists
+    if (!result[supportWorkerId]) {
+      result[supportWorkerId] = {
+        supportWorkerId,
+        name,
+        contractedHours,
+        avatar,
+        visits: []
+      }
+    }
+
+    // update the mapping
+    result[supportWorkerId].visits.push(visitData)
+    return result
+  }, {})
+  return Object.values(transformedData)
+}
+
 function App() {
+  const [loading, setLoading] = useState(true)
   const [supportWorkers, setSupportWorkers] = useState(null)
 
   useEffect(() => {
@@ -14,40 +39,18 @@ function App() {
       try {
         const response = await fetch(url)
         const { data } = await response.json()
-
-        // transform the data by collating worker's visits by supportWorkerId
-        const transformedData = data.reduce((result, visit) => {
-          const {
-            supportWorkerId,
-            name,
-            contractedHours,
-            avatar,
-            ...visitData
-          } = visit
-
-          // create new worker obj if none exists
-          if (!result[supportWorkerId]) {
-            result[supportWorkerId] = {
-              supportWorkerId,
-              name,
-              contractedHours,
-              avatar,
-              visits: []
-            }
-          }
-
-          // update the mapping
-          result[supportWorkerId].visits.push(visitData)
-          return result
-        }, {})
-
-        setSupportWorkers(Object.values(transformedData))
+        setSupportWorkers(transformData(data))
+        setLoading(false)
       } catch (error) {
-        console.error()
+        console.error('Error fetching data:', error)
       }
     }
     fetchData()
   }, [])
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
 
   return (
     <Container>
